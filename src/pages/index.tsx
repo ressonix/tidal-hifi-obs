6e62d10767d6c54a301a192c5296bc204e4affbf
Michael Kinder <me@mwk.email>
2023-04-17 19:02:28 -0400

    change text color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,7 +52,7 @@ export default function Home() {
         <div className="relative mt-4 h-4 rounded-full overflow-hidden bg-gray-500">
           <div className="absolute inset-0 bg-blue-600" style={{ width: `${progress}%` }} />
         </div>
-        <div className="flex justify-between mt-2 text-xs text-gray-500">
+        <div className="flex justify-between mt-2 text-xs text-white">
           <div>{current}</div>
           <div>{duration}</div>
         </div>
